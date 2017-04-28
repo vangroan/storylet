3cda7056381d3035aafddbb519782b78b8b43461
Willem Victor <wimpievictor@gmail.com>
2017-04-28 22:15:02 +0200

    Added StoryAnswer.

--- src/story.rs
+++ src/story.rs
@@ -2,10 +2,22 @@
 use serde::de::Deserialize;
 
 
+
+
+
 #[derive(Deserialize, Debug)]
 pub struct StoryNode {
     pub id: String,
     pub content: String,
+    pub answers: Vec<StoryAnswer>,
 }
+
+
+#[derive(Deserialize, Debug)]
+pub struct StoryAnswer {
+    pub id: String,
+    pub content: String,
+}
+
 
 pub struct StoryRunner {}
